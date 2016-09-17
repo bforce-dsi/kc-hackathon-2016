ce51cb731bcabc0a7bcbbb2e1da5a863a3baceea
Ben <benforce@gmail.com>
2016-09-17 16:00:24 -0500

    Created UDP protocol

--- UnityProject/Assets/Scripts/UDPClient.cs
+++ UnityProject/Assets/Scripts/UDPClient.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 
 public class UDPClient : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    #region Private Fields
+
+    private IPEndPoint serverEndPoint;
+#endregion
+
+    public int port;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
@@ -12,4 +23,24 @@ public class UDPClient : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public void Connect()
+    {
+        var client = new UdpClient();
+        var endPoint = new IPEndPoint(IPAddress.Broadcast, port);
+
+        var connectPacket = new UDPHost.PacketHeader()
+        {
+            id = UDPHost.PacketHeader.PacketTypes.Announce,
+            length = 0
+        };
+
+        var buffer = connectPacket.Encode();
+
+        client.Send(buffer, buffer.Length, endPoint);
+
+        var result = client.Receive(ref endPoint);
+
+        client.Close();
+    }
 }
