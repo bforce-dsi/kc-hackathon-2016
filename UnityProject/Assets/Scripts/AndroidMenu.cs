9e5866fe91f8908a1d4cbf65db5b73adc9365596
Ben <benforce@gmail.com>
2016-09-17 13:45:34 -0500

    Created Android Menu Scene

---
+++ UnityProject/Assets/Scripts/AndroidMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AndroidMenu : MonoBehaviour
+{
+
+    public GameObject OutputLabel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnBtnTest_Click()
+    {
+        var text = OutputLabel.GetComponent<GUIText>();
+
+        if (text != null)
+            text.text = "Button Pressed";
+    }
+}
